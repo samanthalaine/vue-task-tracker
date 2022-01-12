a72d6faf7cef3ffdfe3fdc043e3091bf4de69fcc
samanthalaine <samantha@voicethread.net>
2022-01-12 09:47:36 -0500

    added button

--- task-tracker/src/components/Header.vue
+++ task-tracker/src/components/Header.vue
@@ -1,26 +1,30 @@
 <template>
-    <header>
-        <h1>
-            {{title}}
-        </h1>
-    </header>
+  <header>
+    <h1>
+      {{ title }}
+    </h1>
+    <Button text='Add Task' color='green' />
+  </header>
 </template>
 
 <script>
-    export default {
-        name: 'Header',
-        props: {
-            title: String,
-        },
-    }
-
-</script> 
+import Button from "./Button";
+export default {
+  name: "Header",
+  props: {
+    title: String,
+  },
+  components: {
+    Button,
+  },
+};
+</script>
 
 <style scoped>
 header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
 }
 </style>
